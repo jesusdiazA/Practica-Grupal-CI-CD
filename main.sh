d007d97725af2c570f66ee26e05d20cf700b08c2
jesusdiazA <jesusdiaz4toc@gmail.com>
2021-02-24 17:54:16 +0100

    añadido sh

---
+++ main.sh
@@ -0,0 +1,7 @@
+
+  
+#!/bin/bash 
+echo "Hola Mundo Jenkins soy un script de Linux. Francisco Javier Melero"
+echo "Para practicar tareas sencillas en Jenkins, sin build y sin test, y sin deploy nube"
+echo "Podremos automatizar, manual, hook con github (push)"
+echo "OJORGE......dfsafaffsdff............................................ao"
